a5c81203961be9c2136ad096e1adc52ac99e494d
Cristophe <cristophejunior016@gmail.com>
2024-03-09 12:12:05 -0300

    Indice commit

--- src/teste01/Indice.java
+++ src/teste01/Indice.java
@@ -1,7 +1,43 @@
 package teste01;
 
+import java.util.Scanner;
+
 public class Indice {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Você deseja a função While ou For? ");
+        String escolha = scanner.nextLine();
 
+        switch (escolha){
+            case "While":
+                While();
+                break;
+            case "For":
+                For();
+                break;
+            default:
+                System.out.println("Você escolheu uma opção inválida, ou digitou incorretamente, tente novamente!");
+        }
+
+    }
+
+    public static void While(){
+        int indice=13;
+        int soma=0;
+        int k=0;
+        while(k < indice){
+            soma = soma + k;
+            k ++;
+        }
+        System.out.println(soma);
+    }
+
+    public static void For(){
+        int indice=13;
+        int soma=0;
+        for (int k=0; k < indice; k++){
+            soma = soma + k;
+        }
+        System.out.println(soma);
     }
 }
